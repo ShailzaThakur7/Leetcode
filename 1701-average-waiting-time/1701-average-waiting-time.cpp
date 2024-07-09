a629c4b42cf1c9276c7d14bce1d1642fd157344d
Shailza Thakur <141495173+ShailzaThakur7@users.noreply.github.com>
2024-07-09 23:08:14 +0530

    Time: 143 ms (76.47%), Space: 76.4 MB (83.82%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,51 +1,24 @@
-// class Solution {
-// public:
-//     double averageWaitingTime(vector<vector<int>>& customers) {
-//         int currentTime=0;
-//         int longTime=0;
-        
-//         for(int i=0;i<customers.size();i++){
-//             int arrivalTime=customers[i][0];
-//             int serviceTime=customers[i][1];
-            
-//             if(currentTime<arrivalTime){
-//                 currentTime=arrivalTime;
-//             }
-            
-//             longTime+=(currentTime+serviceTime-arrivalTime);
-            
-//             currentTime+=serviceTime;
-//         }
-        
-//         return static_cast<double>(longTime)/customers.size();
-        
-                
-//     }
-// };
-
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        int currentTime = 0;
-        long long totalWaitingTime = 0;
-
-        for (int i = 0; i < customers.size(); ++i) {
-            int arrivalTime = customers[i][0];
-            int serviceTime = customers[i][1];
-
-            // If the cook is idle until the customer arrives
-            if (currentTime < arrivalTime) {
-                currentTime = arrivalTime;
+        int currentTime=0;
+        long long longTime=0;
+        
+        for(int i=0;i<customers.size();i++){
+            int arrivalTime=customers[i][0];
+            int serviceTime=customers[i][1];
+            
+            if(currentTime<arrivalTime){
+                currentTime=arrivalTime;
             }
-
-            // Calculate the waiting time for the current customer
-            totalWaitingTime += (currentTime + serviceTime - arrivalTime);
-
-            // Update the current time after serving the current customer
-            currentTime += serviceTime;
+            
+            longTime+=(currentTime+serviceTime-arrivalTime);
+            
+            currentTime+=serviceTime;
         }
-
-        // Return the average waiting time
-        return static_cast<double>(totalWaitingTime) / customers.size();
+        
+        return static_cast<double>(longTime)/customers.size();
+        
+                
     }
 };
