3581b5e9f35ce02819cb142666775629f1968dee
Shailza Thakur <141495173+ShailzaThakur7@users.noreply.github.com>
2024-08-04 20:21:10 +0530

    Time: 286 ms (10.39%), Space: 31.2 MB (19.95%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,68 +1,32 @@
-// class Solution {
-// public:
-//     int superEggDrop(int k, int n) {
-//         vector<vector<int>> dp(k+1,vector<int>(n+1,0));
-//         //base cases
-//         for(int j=0;j<=n;j++){
-//             dp[1][j]=j;
-//         }
-//         for(int i=2;i<=k;i++){
-//             for(int j=1;j<=n;j++){
-//                 int low=1,high=j,result=j;
-//                 while(low<=high){
-//                     int mid=low+(high-low)/2;
-//                     int breakCase=dp[i-1][mid-1];
-//                     int notbreakCase=dp[i][j-mid];
-//                     int worstCase=max(breakCase,notbreakCase)+1;
-                    
-//                     if(breakCase>notbreakCase){
-//                         high=mid-1;
-//                     }else{
-//                         high=mid+1;
-//                     }
-                
-//                 result=min(result,worstCase);
-                
-//             }
-//             dp[i][j]=result;
-//             }
-//         }
-//         return dp[k][n];
-//     }
-// };
-
 class Solution {
 public:
     int superEggDrop(int k, int n) {
-        vector<vector<int>> dp(k + 1, vector<int>(n + 1, 0));
-        
-        // Base case: with 1 egg, we need j attempts for j floors
-        for (int j = 1; j <= n; ++j) {
-            dp[1][j] = j;
+        vector<vector<int>> dp(k+1,vector<int>(n+1,0));
+        //base cases
+        for(int j=1;j<=n;j++){
+            dp[1][j]=j;
         }
-        
-        // Fill the dp table
-        for (int i = 2; i <= k; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                int low = 1, high = j, result = j;
-                while (low <= high) {
-                    int mid = low + (high - low) / 2;
-                    int breakCase = dp[i-1][mid-1];
-                    int notBreakCase = dp[i][j-mid];
-                    int worstCase = max(breakCase, notBreakCase) + 1;
+        for(int i=2;i<=k;i++){
+            for(int j=1;j<=n;j++){
+                int low=1,high=j,result=j;
+                while(low<=high){
+                    int mid=low+(high-low)/2;
+                    int breakCase=dp[i-1][mid-1];
+                    int notbreakCase=dp[i][j-mid];
+                    int worstCase=max(breakCase,notbreakCase)+1;
                     
-                    if (breakCase > notBreakCase) {
-                        high = mid - 1;
-                    } else {
-                        low = mid + 1;
+                    if(breakCase>notbreakCase){
+                        high=mid-1;
+                    }else{
+                        low=mid+1;
                     }
-                    
-                    result = min(result, worstCase);
-                }
-                dp[i][j] = result;
+                
+                result=min(result,worstCase);
+                
+            }
+            dp[i][j]=result;
             }
         }
-        
         return dp[k][n];
     }
 };
