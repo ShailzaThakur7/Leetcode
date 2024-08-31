dfca586431e283c5eb01b8911c4a332d3e09024e
Shailza Thakur <141495173+ShailzaThakur7@users.noreply.github.com>
2024-08-31 19:56:04 +0530

    Time: 7 ms (50.97%), Space: 17.7 MB (67.93%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -14,12 +14,11 @@ public:
     int maxDepth(TreeNode* root) {
         if(root==NULL){
             return 0;
-        }
+        }   
         int lh=maxDepth(root->left);
         int rh=maxDepth(root->right);
         
-        return max(lh,rh)+1;      
-        
+        return 1+max(lh,rh);        
         
     }
 };
