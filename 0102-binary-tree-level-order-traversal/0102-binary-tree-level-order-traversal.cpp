6552d5b28d4c24a8800171747c1d20bd8a81732c
Shailza Thakur <141495173+ShailzaThakur7@users.noreply.github.com>
2024-08-31 19:14:13 +0530

    Time: 4 ms (61.44%), Space: 15.3 MB (26.20%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,53 +1,40 @@
-// #include <vector>
-// #include <queue>
-
-// using namespace std;
-
-// // Definition for a binary tree node.
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (root == nullptr) {
-            return result;
+        vector<vector<int>> ans;
+        if(root==NULL){
+            return ans;
         }
-        
         queue<TreeNode*> q;
-        q.push(root); // Push the root node into the queue
-        
-        while (!q.empty()) {
-            int levelSize = q.size(); // Number of nodes at the current level
-            vector<int> levelNodes; // To store nodes at the current level
-            
-            for (int i = 0; i < levelSize; ++i) {
-                TreeNode* node = q.front();
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
                 q.pop();
-                
-                // Add the node's value to the current level vector
-                levelNodes.push_back(node->val);
-                
-                // Add left and right children to the queue if they exist
-                if (node->left != nullptr) {
+                if(node->left!=NULL){
                     q.push(node->left);
                 }
-                if (node->right != nullptr) {
+                if(node->right!=NULL){
                     q.push(node->right);
                 }
+                level.push_back(node->val);
             }
-            
-            // Add the current level vector to the result
-            result.push_back(levelNodes);
+            ans.push_back(level);
         }
+        return ans;
         
-        return result;
     }
 };
